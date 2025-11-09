05c45b7030d3853b7cd9b06f0b7cf78d54a0b42a
Ashvini barode <159224330+ashvinibarode@users.noreply.github.com>
2025-11-09 08:55:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById("display");
+
+function appendNumber(num) {
+  display.value += num;
+}
+
+function appendOperator(op) {
+  const lastChar = display.value.slice(-1);
+  if (!['+', '-', '*', '/', '%'].includes(lastChar)) {
+    display.value += op;
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
